3526c0551bb2ef2276c8cd4d880d33fc7924dea1
sevelinCA <ngabosevelin@gmail.com>
2024-06-11 12:09:29 +0200

    add webhook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,101 @@
 const express = require('express')
-const checkoutRoute = require('./routes/payment')
 const app = express()
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 app.use(bodyParser.json());
 app.use(express.json())
 
-app.use("/", checkoutRoute)
+
+
+
+app.get("/success", (req, res) => {
+    res.send("Successfully");
+});
+app.get("/cancel", (req, res) => {
+    res.send("canceled");
+});
+
+app.post("/checkout", async (req, res) => {
+  try {
+    const { cartData } = req.body;
+    let line_items = [];
+    cartData.map((item) => {
+        line_items.push({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: item.name,
+                    images: [item.image]
+                },
+                unit_amount: item.price
+            },
+            quantity: item.quantity,
+            metadata: {
+              vendorId: item.vendorId
+            }
+        });
+    });
+    const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode: 'payment',
+        success_url: 'https://payment-p5w9.onrender.com/success',
+        cancel_url: 'https://payment-p5w9.onrender.com/cancel',
+    });
+
+    res.json({ sessionId: session.id, url: session.url });
+    
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.post('/webhook', bodyParser.raw({ type: 'application/json' }), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+  const webhookSecret = "whsec_KUYyC7TzJgrNT3nAAk1SBFBTp1ALt1AX";
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, webhookSecret);
+  } catch (err) {
+    console.log(`⚠️  Webhook signature verification failed.`, err.message);
+    return response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const session = event.data.object;
+      // Log the session data to console
+      console.log('Session completed: ', session);
+      break;
+    case 'checkout.session.async_payment_succeeded':
+      const checkoutsessionasync_payment_succeeded = event.data.object;
+      // Log the payment intent data to console
+      console.log('checkout session async_payment_succeeded: ', checkoutsessionasync_payment_succeeded);
+      break;
+    case 'checkout.session.expired':
+      const checkoutsessionexpired = event.data.object;
+      // Log the payment method data to console
+      console.log('checkout session expired: ', checkoutsessionexpired);
+      break;
+      case 'checkout.session.async_payment_failed':
+        const checkoutSessionAsyncPaymentFailed = event.data.object;
+      console.log('checkout session expired: ', checkoutSessionAsyncPaymentFailed);
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({ received: true, webhookId: event.id });
+});
+
+
 
 
 
