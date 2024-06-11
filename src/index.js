b01b8a9de8ec03ec370273c4207092039f6c2609
sevelinCA <ngabosevelin@gmail.com>
2024-06-11 12:28:57 +0200

    add webhook

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const checkoutRoute = require('./routes/payment')
 const app = express()
 
+
 app.use(express.json())
 
 app.use("/", checkoutRoute)
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -51,43 +51,10 @@ router.post('/webhook', express.raw({ type: 'application/json' }), (request, res
   const sig = request.headers['stripe-signature'];
   const webhookSecret = "whsec_KUYyC7TzJgrNT3nAAk1SBFBTp1ALt1AX";
 
-  let event;
+  let event = req.body
+  console.log(event)
 
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, webhookSecret);
-  } catch (err) {
-    console.log(`⚠️  Webhook signature verification failed.`, err.message);
-    return response.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  // Handle the event
-  switch (event.type) {
-    case 'checkout.session.completed':
-      const session = event.data.object;
-      // Log the session data to console
-      console.log('Session completed: ', session);
-      break;
-    case 'checkout.session.async_payment_succeeded':
-      const checkoutsessionasync_payment_succeeded = event.data.object;
-      // Log the payment intent data to console
-      console.log('checkout session async_payment_succeeded: ', checkoutsessionasync_payment_succeeded);
-      break;
-    case 'checkout.session.expired':
-      const checkoutsessionexpired = event.data.object;
-      // Log the payment method data to console
-      console.log('checkout session expired: ', checkoutsessionexpired);
-      break;
-      case 'checkout.session.async_payment_failed':
-        const checkoutSessionAsyncPaymentFailed = event.data.object;
-      console.log('checkout session expired: ', checkoutSessionAsyncPaymentFailed);
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a response to acknowledge receipt of the event
-  response.json({ received: true, webhookId: event.id });
+  
 });
 
 module.exports = router;
