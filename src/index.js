482c440070f91cb0a518e1779bf2820bf0ee87bf
sevelinCA <ngabosevelin@gmail.com>
2024-06-12 16:08:03 +0200

    add webhook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const checkoutRoute = require('./routes/payment')
 const app = express()
+const bodyParser = require("body-parser")
 
-
+app.use(bodyParser.raw())
+app.use(express.raw())
 app.use(express.json())
 
 app.use("/", checkoutRoute)
